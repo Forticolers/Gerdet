4470d02cb86972f65429d03832a078485443f9dc
Forticolers <julien.jeanbourquin@hotmail.com>
2015-03-11 22:47:45 +0100

    First Commit

---
+++ Gerdet/src/ch/Forticolers/Gerdet/Main.java
@@ -0,0 +1,53 @@
+package ch.Forticolers.Gerdet;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		//Variable ici
+		
+		//Codes ici
+		
+		return false;
+	}
+
+	@Override
+	public void onDisable() {
+		//Variable ici
+		
+		//Codes ici
+		
+	}
+
+	@Override
+	public void onEnable() {
+		//Variable ici
+		
+		//Codes ici
+		
+	}
+
+	@Override
+	public void onLoad() {
+		//Variable ici
+		
+		//Codes ici
+		
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event){
+		//Variable ici
+		
+		//Codes ici
+		
+	}
+
+}
