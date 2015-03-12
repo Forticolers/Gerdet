7bfecbfdc20cc994eb93dd374972f97bde1b1581
Loucool111 <Loucool111@gmail.com>
2015-03-12 18:41:29 +0100

    ajout du VI

--- Gerdet/src/ch/Forticolers/Gerdet/Inventory/InventoryCreate.java
+++ Gerdet/src/ch/Forticolers/Gerdet/Inventory/InventoryCreate.java
@@ -1,8 +1,17 @@
 package ch.Forticolers.Gerdet.Inventory;
 
-public class InventoryCreate {
-	
-	public InventoryCreate(){
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class InventoryCreate 
+{	
+	public InventoryCreate()
+	{
 		
 	}
+	
+	public Inventory initOnFirstLogin(Player player)
+	{
+		return null;	
+	}	
 }
---
+++ Gerdet/src/ch/Forticolers/Gerdet/Inventory/InventoryVI.java
@@ -0,0 +1,43 @@
+package ch.Forticolers.Gerdet.Inventory;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import ch.Forticolers.Gerdet.VirtualInventory.IVirtualInventory;
+
+public class InventoryVI implements IVirtualInventory 
+{
+	Inventory inventory;
+	Player owner;
+	
+	@Override
+	public Inventory getInventory() 
+	{
+		return this.inventory;
+	}
+
+	@Override
+	public Player getOwner() 
+	{
+		return this.owner;
+	}
+
+	@Override
+	public Inventory createInventory(ItemStack[] items) 
+	{
+		this.inventory = Bukkit.createInventory(getOwner(), 9,ChatColor.UNDERLINE + "Inv");
+		
+		this.inventory.setContents(items);
+		
+		return this.inventory;
+	}
+
+	@Override
+	public void showInventory(Player player, Inventory inventory) 
+	{
+		player.openInventory(inventory);
+	}
+}
