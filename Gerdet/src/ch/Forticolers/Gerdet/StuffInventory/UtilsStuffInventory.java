b043862acea588ab338d13524d13d0020092db1b
Forticolers <julien.jeanbourquin@hotmail.com>
2015-03-13 22:47:19 +0100

    asdadasdasdasd

--- Gerdet/src/ch/Forticolers/Gerdet/StuffInventory/UtilsStuffInventory.java
+++ Gerdet/src/ch/Forticolers/Gerdet/StuffInventory/UtilsStuffInventory.java
@@ -12,38 +12,38 @@ public class UtilsStuffInventory
 				
 		ItemStack[] armor = new ItemStack[36];
 		
-		armor[0] = sep;
+		armor[0] = new ItemStack(Material.DIAMOND_HELMET);
 		armor[1] = sep;
 		armor[2] = sep;
 		armor[3] = sep;
-		armor[4] = new ItemStack(Material.DIAMOND_SWORD);
+		armor[4] = sep;
 		armor[5] = sep;
 		armor[6] = sep;
 		armor[7] = sep;
 		armor[8] = sep;
-		armor[9] = sep;
-		armor[10] = sep;
+		armor[9] = new ItemStack(Material.DIAMOND_CHESTPLATE);
+		armor[10] = 
 		armor[11] = sep;
 		armor[12] = sep;
-		armor[13] = new ItemStack(Material.DIAMOND_SWORD);
+		armor[13] = sep;
 		armor[14] = sep;
 		armor[15] = sep;
 		armor[16] = sep;
 		armor[17] = sep;
-		armor[18] = sep;
+		armor[18] = new ItemStack(Material.DIAMOND_LEGGINGS);
 		armor[19] = sep;
 		armor[20] = sep;
 		armor[21] = sep;
-		armor[22] = new ItemStack(Material.DIAMOND_SWORD);
+		armor[22] = sep;
 		armor[23] = sep;
 		armor[24] = sep;
 		armor[25] = sep;
 		armor[26] = sep;
-		armor[27] = sep;
+		armor[27] = new ItemStack(Material.DIAMOND_BOOTS);
 		armor[28] = sep;
 		armor[29] = sep;
 		armor[30] = sep;
-		armor[31] = new ItemStack(Material.DIAMOND_SWORD);
+		armor[31] = sep;
 		armor[32] = sep;
 		armor[33] = sep;
 		armor[34] = sep;
@@ -53,8 +53,17 @@ public class UtilsStuffInventory
 		return armor;		
 	}
 	public static ItemStack[] GetStuffFromPlay(Player player){
+		ItemStack sep = new ItemStack(Material.STAINED_GLASS_PANE,1,(short)15);
+		
 		ItemStack[] stuff = new ItemStack[45];
 		
+		stuff[28] = sep;
+		stuff[29] = sep;
+		stuff[31] = sep;
+		stuff[32] = sep;
+		stuff[33] = sep;
+		stuff[34] = sep;
+		stuff[35] = sep;
 		return stuff;		
 	}
 }
