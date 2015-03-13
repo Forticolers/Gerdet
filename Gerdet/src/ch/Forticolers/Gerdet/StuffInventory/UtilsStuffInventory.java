a14246784bd2876d8d00caaff4909e7aff8a4cb7
Loucool111 <Loucool111@gmail.com>
2015-03-13 22:12:24 +0100

    aaaa

--- Gerdet/src/ch/Forticolers/Gerdet/StuffInventory/UtilsStuffInventory.java
+++ Gerdet/src/ch/Forticolers/Gerdet/StuffInventory/UtilsStuffInventory.java
@@ -7,10 +7,9 @@ import org.bukkit.inventory.ItemStack;
 public class UtilsStuffInventory {
 	public static ItemStack[] GetArmorFromPlay(Player player){
 		ItemStack[] armor = new ItemStack[36];
-		ItemStack separateur = new ItemStack(Material.THIN_GLASS);
-		separateur.setDurability((short)3);
+		
 		for(ItemStack i : armor){
-			i = separateur;
+			i = new ItemStack(Material.THIN_GLASS);
 		}
 		
 		return armor;		
