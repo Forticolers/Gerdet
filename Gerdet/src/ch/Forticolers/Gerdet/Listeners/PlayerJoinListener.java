ef9b23b43be6d33960606d03c37db5c86d252194
Loucool111 <Loucool111@gmail.com>
2015-03-15 16:06:24 +0100

    changement des files on va utiliser le Yaml vu que c'est beaucoup plus simple.

--- Gerdet/src/ch/Forticolers/Gerdet/Listeners/PlayerJoinListener.java
+++ Gerdet/src/ch/Forticolers/Gerdet/Listeners/PlayerJoinListener.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -28,52 +30,44 @@ public class PlayerJoinListener implements Listener
 		//Variables ici
 		final Player player = event.getPlayer();		
 		//Codes ici
-			//Message bienvenue
-			Bukkit.getScheduler().scheduleSyncDelayedTask(pl, new Runnable(){			
-				@Override
-				public void run()
-				{
-					player.sendMessage(ChatColor.AQUA+"Bienvenue, "+ player.getName() + "\nFaite /help pour voir \nles commandes qui vont ont été attribuées");
-				}
-			}, 10);
-			//Création du dossier inventaire
-			File dossier;		
-			dossier = new File (Bukkit.getPluginManager().getPlugin("Gerdet").getDataFolder()+"\\"+player.getName());
-			if(dossier.exists())
+		//Message bienvenue
+		Bukkit.getScheduler().scheduleSyncDelayedTask(pl, new Runnable(){			
+			@Override
+			public void run()
 			{
-				pl.getLogger().info("Directory already existed !");
+				player.sendMessage(ChatColor.AQUA+"Bienvenue, "+ player.getName() + "\nFaite /help pour voir \nles commandes qui vont ont été attribuées");
 			}
-			else
-			{
-				dossier.mkdirs();
-				pl.getLogger().info("Directory succesfully created !");
-			}
-			//Création des fichier stuff et armure
-			File fileArmor, fileStuff;	
-			fileArmor = new File(Bukkit.getPluginManager().getPlugin("Gerdet").getDataFolder() +"\\" + player.getName() +"\\"+player.getName()+"_Armor.txt");
-			fileStuff = new File(Bukkit.getPluginManager().getPlugin("Gerdet").getDataFolder() +"\\" + player.getName() +"\\"+player.getName()+"_Stuff.txt");
-			//armure
-			if(!(fileArmor.exists()))
-			{
-				try
-				{
-					fileArmor.createNewFile();
-				}
-				catch(IOException e)
-				{
-					e.printStackTrace();
-				}
-			}
-			//stuff
-			if(!(fileStuff.exists())){
-				try
-				{
-					fileStuff.createNewFile();
-				}
-				catch(IOException e)
-				{
-					e.printStackTrace();
-				}				
-			}
+		}, 10);
+		
+		//Création du dossier inventaire
+		File dossier;		
+		dossier = new File (Bukkit.getPluginManager().getPlugin("Gerdet").getDataFolder()+"\\"+player.getName());
+		if(dossier.exists())
+		{
+			pl.getLogger().info("Directory already existed !");
+		}
+		else
+		{
+			dossier.mkdirs();
+			pl.getLogger().info("Directory succesfully created !");
+		}
+		
+		//Création des fichier stuff et armure
+		File fileArmor, fileStuff;	
+		fileArmor = new File(Bukkit.getPluginManager().getPlugin("Gerdet").getDataFolder() +"\\" + player.getName() +"\\"+player.getName()+"_Armor.yml");
+		fileStuff = new File(Bukkit.getPluginManager().getPlugin("Gerdet").getDataFolder() +"\\" + player.getName() +"\\"+player.getName()+"_Stuff.yml");
+		
+		FileConfiguration armorYaml = YamlConfiguration.loadConfiguration(fileArmor);
+		FileConfiguration stuffYaml = YamlConfiguration.loadConfiguration(fileStuff);
+					
+		try 
+		{
+			armorYaml.save(fileArmor);
+			stuffYaml.save(fileStuff);
+		} 
+		catch (IOException e1) 
+		{
+			e1.printStackTrace();
+		}
 	}
 }
